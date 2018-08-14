0e4b7195e395ccc6d29c508f32543de64bdde59b
farhanpro <31347917+farhanpro@users.noreply.github.com>
2018-08-14 23:51:25 +0530

    Update bottels.java

--- bottels.java
+++ bottels.java
@@ -2,21 +2,21 @@ public class Main {
 
     public static void main(String[] args) {
 	int bottels=99;
-	String word =" bottels";
+	String word =" Bottels";
 	    while(bottels>0)
         {
             if (bottels==1)
             {
                 word="bottel";
             }
-            System.out.println((bottels-1) +"" +word+"In the wall");
+            System.out.println((bottels-1) +" " +word+"In the wall");
             System.out.println((bottels-1)+""+word+"Down");
             System.out.println("Take one Down");
             System.out.println("Pass it around");
-            bottels=bottels-1;
+            bottels-=1;
             if (bottels>0)
             {
-                System.out.println(bottels+""+word+""+"of beer on the wall");
+                System.out.println(bottels+" "+word+" "+"of beer on the wall");
             }
             if(bottels==0)
             {
